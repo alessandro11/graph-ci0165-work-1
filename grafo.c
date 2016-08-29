ade53e2279cb2a74dcb2f25bdf7f1b1e4f58580c
Alessandro Elias <ae11@c3sl.ufpr.br>
2016-08-28 21:23:08 -0300

    Started function constroi_grafo().
    Signed-off-by: Alessandro Elias <ae11@c3sl.ufpr.br>

--- grafo.c
+++ grafo.c
@@ -19,7 +19,6 @@
 #include <malloc.h>
 #include "grafo.h"
 
-
 //-----------------------------------------------------------------------------
 // (apontador para) lista encadeada
 typedef struct lista *lista;
@@ -81,29 +80,6 @@ int remove_no(struct lista *l, struct no *rno, int destroi(void *));
 //______________________________________________________________________________
 
 
-
-//------------------------------------------------------------------------------
-// lê um grafo no formato dot de input, usando as rotinas de libcgraph
-//
-// desconsidera todos os atributos do grafo lido exceto o atributo
-// "peso" quando ocorrer; neste caso o valor do atributo é o peso da
-// aresta/arco que é um long int
-//
-// num grafo com pesos todas as arestas/arcos tem peso
-//
-// o peso default é 0
-//
-// todas as estruturas de dados alocadas pela libcgraph são
-// desalocadas ao final da execução
-//
-// devolve o grafo lido
-//      ou NULL em caso de erro
-
-grafo le_grafo(FILE *input) {
-
-	return NULL;
-}
-
 //------------------------------------------------------------------------------
 // devolve o nome do grafo g
 char *nome_grafo(grafo g) {
@@ -157,7 +133,6 @@ int destroi_grafo(grafo g) {
 	return 0;
 }
 
-
 //______________________________________________________________________________
 //
 // Definição das funções que trata a lista.
@@ -165,8 +140,9 @@ int destroi_grafo(grafo g) {
 // nó de lista encadeada cujo conteúdo é um void *
 struct no {
 
-  void *conteudo;
-  no proximo;
+  void*	conteudo;
+  no	anterior;
+  no 	proximo;
 };
 
 //---------------------------------------------------------------------------
@@ -174,7 +150,7 @@ struct no {
 struct lista {
 
   unsigned int tamanho;
-  //int padding; // só pra evitar warning
+  int padding; // só pra evitar warning
   no primeiro;
 };
 
@@ -294,4 +270,135 @@ int remove_no(struct lista *l, struct no *rno, int destroi(void *)) {
 	return 0;
 }
 
+/*
+ *******************************************************************************
+ * Meu código começa aqui.
+ *******************************************************************************
+ */
 
+#include <graphviz/cgraph.h>
+#define _XOPEN_SOURCE
+#include <string.h>
+
+typedef unsigned int uint;
+typedef long int lint;
+typedef unsigned short ushort;
+typedef int bool;
+
+#ifndef TRUE
+#define TRUE            1
+#endif
+
+#ifndef FALSE
+#define FALSE           0
+#endif
+
+#ifndef NULL
+#define NULL			0
+#endif
+
+#define ERRO(fmt, ...)       (fprintf(stderr, (fmt), ## __VA_ARGS__))
+
+struct grafo {
+	uint 	nvertices;
+	uint 	narestas;
+	bool	ponderado;
+	int		padding;
+	char*	nome;
+	lista	vertices;
+};
+
+struct vertice {
+	char*	nome;
+	lista	vizinhos_esq;
+	lista	vizinhos_dir;
+};
+typedef struct vertice* vertice;
+
+struct aresta {
+	char*	nome;
+	vertice	origem;
+	vertice	destino;
+};
+typedef struct aresta* aresta;
+
+grafo* alloc_grafo(void);
+char* str_dup(const char* str);
+
+
+
+grafo* alloc_grafo(void) {
+	grafo* g = (grafo*)calloc(1, sizeof(struct grafo));
+	return g;
+}
+
+char* str_dup(const char* str) {
+	char* p = (char*)calloc(1, strlen(str)+1);
+	if( p )
+		strcpy(p, str);
+
+	return p;
+}
+//------------------------------------------------------------------------------
+// lê um grafo no formato dot de input, usando as rotinas de libcgraph
+//
+// desconsidera todos os atributos do grafo lido exceto o atributo
+// "peso" quando ocorrer; neste caso o valor do atributo é o peso da
+// aresta/arco que é um long int
+//
+// num grafo com pesos todas as arestas/arcos tem peso
+//
+// o peso default é 0
+//
+// todas as estruturas de dados alocadas pela libcgraph são
+// desalocadas ao final da execução
+//
+// devolve o grafo lido
+//      ou NULL em caso de erro
+grafo le_grafo(FILE *input) {
+    Agraph_t*	Ag_g;
+    grafo       g;
+
+    g = (grafo)alloc_grafo();
+    Ag_g = agread(input, NULL);
+    if ( !Ag_g ) {
+        free(g);
+        ERRO("Não foi possível ler o grado!\n");
+        return NULL;
+    }
+
+    g->nome = str_dup(agnameof(Ag_g));
+    g->ponderado = agisdirected(Ag_g);
+    g->nvertices= (uint)agnnodes(Ag_g);
+    g->narestas = (uint)agnedges(Ag_g);
+    g->vertices = constroi_lista();
+    constroi_grafo(Ag_g, g);
+    /*
+    for( Ag_v=agfstnode(Ag_g); Ag_v; Ag_v=agnxtnode(Ag_g, Ag_v) ) {
+        v = (vertice)mymalloc(sizeof(struct vertice));
+        memset(v, 0, sizeof(struct vertice));
+        v->v_nome = strdup(agnameof(Ag_v));
+        v->v_lbl  = (int*)mymalloc(sizeof(int) * g->g_nvertices);
+                memset(v->v_lbl, 0, sizeof(int) * g->g_nvertices);
+        v->v_neighborhood_in = constroi_lista();
+        v->v_neighborhood_out = constroi_lista();
+        // Insert vertex to the list of vertexes in the graph list.
+        if( !insere_lista(v, g->g_vertices) ) exit(EXIT_FAILURE);
+    }
+
+    BuildList build_list[2];
+    build_list[0] = BuildListOfEdges;
+    build_list[1] = BuildListOfArrows;
+    for( Ag_v=agfstnode(Ag_g); Ag_v; Ag_v=agnxtnode(Ag_g, Ag_v) )
+        build_list[g->g_tipo](g, Ag_g, Ag_v, agnameof(Ag_v));
+    */
+    agclose(Ag_g);
+    return g;
+}
+
+void constroi_grafo(Agraph_t* ag, grafo g) {
+	for( Agnode_t* v=agfstnode(ag); v; v=agnxtnode(ag, v) ) {
+		ver = alloc_vertice(agnameof(v));
+
+	}
+}
